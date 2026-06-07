233462e007305306c5bd7f065e8ec0ce712efa3a
Jesky-Sean <jesky.sean@gmail.com>
2026-06-07 08:54:47 -0600

    Add files via upload
    Python code to read serial data from Serial ports of an arduino 
    
    Using pip install pyserial

---
+++ SERIAL_DATA/PysSerialReader/serialRead.py
@@ -0,0 +1,12 @@
+import serial
+
+#Windows
+#PORT COM16
+#LINUX
+PORT = "/dev/ttyUSB0"
+
+serialR = serial.Serial(PORT, 921600)
+
+with open("serialData.txt", "ab", buffering=((1024*2)*2)) as files:
+	while True:
+		files.write(serialR.readline())
