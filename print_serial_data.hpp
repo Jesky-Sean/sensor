051fa5f27645cc259e1fd1c95a24eb402497e892
Jesky-Sean <jesky.sean@gmail.com>
2026-06-06 20:19:59 -0600

    Delete print_serial_data.hpp

--- print_serial_data.hpp
+++
@@ -1,5 +0,0 @@
-//pring data recived and inputted from serial pins into function
-template <typename T> void println_raw_data(T& print_this_raw_data, signed short& decimal_places)
-{
-        Serial.println(print_this_raw_data, decimal_places);
-}
